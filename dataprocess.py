3db13120d258bfbf9913fd8a22867b1d211dce13
Liucong-sdu <2765692587@QQ.COM>
2025-05-09 20:21:12 +0800

    暂时写着

--- dataprocess.py
+++ dataprocess.py
@@ -1,3 +1,16 @@
-import pandas as pd
-df= pd.read_csv('train.csv')
-df.describe().to_csv('train_summary.csv')
+import pandas as pd 
+"""df= pd.read_csv('train.csv')
+df.describe().to_csv('train_summary.csv')"""
+import featuretools as ft
+'''初始化一个EntitySet对象'''
+df=pd.read_csv('train.csv')
+es=ft.EntitySet(id='id')
+es = es.add_dataframe(dataframe_name='train', dataframe=df, index='id')
+"""合成体脂率和bmi特征"""
+def Body_Fat_Percentage(BFP):
+    return 1.20 * BFP + 0.23 * 25 - 5.4
+def BMI(BMI):
+    return 1.20 * BMI + 0.23 * 25 - 5.4
+"""通过新合成的数据，使用featuretools进行特征工程"""
+def feature_engineering(df):
+    df
