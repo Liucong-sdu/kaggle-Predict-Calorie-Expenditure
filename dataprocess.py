6e647510430923069d8ad815df7eb6013b29d640
Liucong-sdu <2765692587@QQ.COM>
2025-05-09 20:27:54 +0800

    再次更改

--- dataprocess.py
+++ dataprocess.py
@@ -3,14 +3,20 @@ import pandas as pd
 df.describe().to_csv('train_summary.csv')"""
 import featuretools as ft
 '''初始化一个EntitySet对象'''
-df=pd.read_csv('train.csv')
-es=ft.EntitySet(id='id')
-es = es.add_dataframe(dataframe_name='train', dataframe=df, index='id')
+df1=pd.read_csv('train.csv')
+df2=pd.read_csv('test.csv')
+es1=ft.EntitySet(id='id')
+es2=ft.EntitySet(id='id')
+"""将数据集添加到EntitySet中"""
+es1 = es1.add_dataframe(dataframe_name='train', dataframe=df1, index='id')
+es2 = es2.add_dataframe(dataframe_name='test', dataframe=df2, index='id')
 """合成体脂率和bmi特征"""
-def Body_Fat_Percentage(BFP):
-    return 1.20 * BFP + 0.23 * 25 - 5.4
+def Body_Fat_Percentage(df):
+    return 1.20 * df+ 0.23 * 25 - 5.4
 def BMI(BMI):
     return 1.20 * BMI + 0.23 * 25 - 5.4
-"""通过新合成的数据，使用featuretools进行特征工程"""
+"""通过新合成的数据，使用featuretools进行特征工程并且编码"""
 def feature_engineering(df):
     df
+'''存储为新的csv文件'''
+
